4b0f842688a52cdf97614ad8d65d2f9201eeef1a
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 13:53:23 +0530

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace loops
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i=100;i>=0;i=i-3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
